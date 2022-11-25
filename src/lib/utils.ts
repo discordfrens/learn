24c749210bcd99969d178107a2459c9a487795e9
Nicholas <contactnicholas81@gmail.com>
2022-11-25 20:01:48 +0000

    refactor: improve ui

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,7 +1,7 @@
 export const FormatString = (string: string) => {
-  return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase()
-}
+  return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
+};
 
 export const RemoveDupelicates = (a: any[]) => {
-  return [...new Set(a)]
-}
+  return [...new Set(a)];
+};
